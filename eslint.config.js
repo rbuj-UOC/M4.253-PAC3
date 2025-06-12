51a1bae3e5595f93423aacb5d2d5fcba3348cab0
rbuj <robert.buj@gmail.com>
2025-06-12 20:13:26 +0200

    honor: npm create vite@latest

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'indent': ['error', 2],
+      'prefer-const': 'error',
+      'semi': 'error',
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+npm create vite@latest
+npm install --save-dev eslint-config-prettier eslint-config-recommended prettier
+npm install --save-dev jest @testing-library/react @testing-library/jest-dom jest-environment-jsdom @testing-library/user-event
+npm install --save-dev babel-jest @babel/preset-env @babel/preset-react
+npm install --save-dev whatwg-fetch
